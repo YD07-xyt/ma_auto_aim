6436f2d943a06cfefb68aaceb377d77f64dfafe8
YD07_wd <xyt15080656916@qq.com>
2025-07-25 18:31:01 +0800

    MA

--- fox.sh
+++
@@ -1,4 +0,0 @@
-source install/setup.bash
-ros2 launch foxglove_bridge foxglove_bridge_launch.xml 
-
-
--- run.sh
+++
@@ -1,3 +0,0 @@
-sudo chmod 777 /dev/ttyACM0
-source install/setup.bash
-ros2 launch rm_bringup bringup.launch.py
